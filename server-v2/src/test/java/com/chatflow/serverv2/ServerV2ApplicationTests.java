efccfddfe96f654776366d0d0c901ecb5756478b
Jeremy <jeremykaku@outlook.com>
2025-10-26 22:33:30 -0700

    Create the server-v2

---
+++ server-v2/src/test/java/com/chatflow/serverv2/ServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatflow.serverv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
